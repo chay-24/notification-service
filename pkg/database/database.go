2d2579ebadcc9235002a48ad5929d15d7d4df07a
chay-24 <chaitanyauge1@gmail.com>
2025-04-19 19:06:23 +0530

    fixed timestamp variable

--- pkg/database/database.go
+++ pkg/database/database.go
@@ -21,7 +21,7 @@ func InitDB() (*sqlx.DB, error) {
 func Get(db *sqlx.DB) ([]Notification, error) {
 	var notifications []Notification
 
-	if err := db.Select(&notifications, "SELECT * FROM notifications ORDER BY time DESC LIMIT 100"); err != nil {
+	if err := db.Select(&notifications, "SELECT * FROM notifications ORDER BY timestamp DESC LIMIT 100"); err != nil {
 		log.Println("Error fetching notifications:", err)
 
 		return nil, err
@@ -33,7 +33,7 @@ func Get(db *sqlx.DB) ([]Notification, error) {
 // Save stores the notification in the database.
 func Save(db *sqlx.DB, source, title, message string) error {
 	_, err := db.Exec(
-		"INSERT INTO notifications (source, title, message, time) VALUES (?, ?, ?, datetime('now'))",
+		"INSERT INTO notifications (source, title, message, timestamp) VALUES (?, ?, ?, datetime('now'))",
 		source, title, message)
 
 	if err != nil {
